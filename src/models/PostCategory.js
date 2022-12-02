e33f6af185277b591742e7caa304ac9f2fe889d6
Mayara Marques <mayaramarquescosta@outlook.com>
2022-12-02 16:01:40 -0300

    chore: Requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+  },
+},
+  {
+    timestamps: false,
+    tableName: 'posts_categories',
+    underscored: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      through: PostCategory,
+      foreignKey: "postId",
+      otherKey: 'CategoryId',
+      as: 'Category',
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      through: PostCategory,
+      foreignKey: "categoryId",
+      otherKey: 'postId',
+      as: 'Post',
+    });
+  }
+
+  return PostCategory ;
+
+};
